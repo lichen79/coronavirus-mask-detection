c2173ef37d11a05837a85e74c73e49ecc95606b4
lichen79 <lichen79@gmail.com>
2020-12-05 14:40:03 +0000

    add try catch exception management

---
+++ xml_to_csv.py
@@ -0,0 +1,43 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        #print('xml_file: '+str(xml_file))
+        for member in root.findall('object'):
+            try:
+                value = (root.find('filename').text,
+                         int(root.find('size')[0].text),
+                         int(root.find('size')[1].text),
+                         member[0].text,
+                         int(member[4][0].text),
+                         int(member[4][1].text),
+                         int(member[4][2].text),
+                         int(member[4][3].text)
+                         )
+                xml_list.append(value)
+                break
+            except TypeError:
+                print('TypeError: '+ str(xml_file))
+            #xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+
+def main():
+    for folder in ['train','test']:
+        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+        print('image_path: '+image_path)
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        print('Successfully converted xml to csv.')
+
+
+main()
